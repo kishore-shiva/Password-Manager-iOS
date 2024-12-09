8d8b73a87cd0cdaf1d252680a64ffc16f6c96b07
kishore-shiva <kishoreshiva.s@gmail.com>
2024-12-08 20:15:11 -0600

    Initial Commit

---
+++ Password Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Password Manager
+//
+//  Created by Kishore Shiva Saravanan on 08/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Password Manager/Password_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ManagerApp.swift
+//  Password Manager
+//
+//  Created by Kishore Shiva Saravanan on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Password_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
